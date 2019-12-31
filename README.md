1ac70d28c9b178f195ab999ea7f0fa8dfb434f1f
forecho <caizhenghai@gmail.com>
2019-12-31 11:37:08 +0800

    fix: 增强同步之后可以执行指定命令功能

--- README.md
+++ README.md
@@ -7,8 +7,18 @@
 使用配置文件
 
 ```sh
-cp config config.json
+cp config_example.json config.json
 ```
 
 修改配置文件内容
 
+
+## 配置服务器
+
+本地或者此工具所放置的服务器生成 `ssh` 密钥：
+
+```sh
+ssh-keygen -t rsa -b 4096 -C "for_deploy" -f ~/.ssh/for_deploy
+```
+
+然后把 `~/.ssh/for_deplo.pub` 文件内容依次拷贝到 `config` 配置的服务器的 `~/.ssh/authorized_keys` 文件中
--- main.go
+++ main.go
@@ -14,8 +14,10 @@ type Config struct {
 	GitURL              string `json:"git_url"`               // git 链接
 	GithubWebhookSecret string `json:"github_webhook_secret"` // GitHub webhooks 密钥
 	CloneAfterCmd       string `json:"clone_after_cmd"`       // git clone 之后执行的命令
+	SSHKeyPath          string `json:"ssh_key_path"`          // SSH 登录服务器的密钥文件
 	Server              []struct {
-		RsyncRemotePath string `json:"rsync_remote_path"` // 远程路径
+		SSH             string `json:"ssh"`               // ssh 登录信息
+		RsyncRemotePath string `json:"rsync_remote_path"` // 远程服务器项目路径
 		SyncAfterCmd    string `json:"sync_after_cmd"`    // 同步文件之后执行的命令
 	} `json:"server"`
 }
@@ -28,12 +30,17 @@ func main() {
 	hook, _ := github.New(github.Options.Secret(conf.GithubWebhookSecret))
 
 	http.HandleFunc("/webhooks", func(w http.ResponseWriter, r *http.Request) {
+		// 方便调试
+		// updateCode(conf)
+
 		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
 		if err != nil {
 			if err == github.ErrEventNotFound {
 				// ok event wasn;t one of the ones asked to be parsed
 			}
 		}
+		fmt.Println("收到请求")
+
 		switch payload.(type) {
 
 		case github.ReleasePayload:
@@ -51,12 +58,17 @@ func updateCode(conf *Config) {
 	gitClone(conf.GitURL, webName)
 	fmt.Println(conf.GitURL)
 
-	// cmd(conf.CloneAfterCmd)
+	if len(conf.CloneAfterCmd) > 0 {
+		command(fmt.Sprintf("cd %s  && %s", webName, conf.CloneAfterCmd))
+	}
 
 	for _, value := range conf.Server {
 		fmt.Println(value.RsyncRemotePath)
-		rsync(value.RsyncRemotePath, webName)
-		cmd(value.SyncAfterCmd)
+		sshRemotePath := fmt.Sprintf("%s:%s", value.SSH, value.RsyncRemotePath)
+		rsync(conf, sshRemotePath, webName)
+
+		sshCmd := fmt.Sprintf("ssh -i %s %s \"%s\"", conf.SSHKeyPath, value.SSH, value.SyncAfterCmd)
+		command(sshCmd)
 		fmt.Println("====================================")
 		fmt.Println("================成功=================")
 		fmt.Println("====================================")
@@ -64,15 +76,15 @@ func updateCode(conf *Config) {
 }
 
 func gitClone(gitURL string, webName string) {
-	cmd("rm -rf " + webName)
-	cmd(fmt.Sprintf("git clone --depth=1 %s %s", gitURL, webName))
+	command("rm -rf " + webName)
+	command(fmt.Sprintf("git clone --depth=1 %s %s", gitURL, webName))
 }
 
-func rsync(rsyncRemotePath string, webName string) {
-	cmd(fmt.Sprintf("rsync -av ./%s/* %s", webName, rsyncRemotePath))
+func rsync(conf *Config, sshRemotePath string, webName string) {
+	command(fmt.Sprintf("rsync -avuz -e 'ssh -i %s' ./%s/* %s", conf.SSHKeyPath, webName, sshRemotePath))
 }
 
-func cmd(cmd string) {
+func command(cmd string) {
 	out, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		fmt.Printf("Failed to execute command: %s", cmd)
